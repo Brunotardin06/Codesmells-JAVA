eec26cfbd50d7d7f939812d541b53a9184ae751c
Brunotardin06 <btardinfernandes@gmail.com>
2025-09-08 19:27:20 -0300

    Update DebugIcon.java

--- Clean/DebugIcon.java
+++ Clean/DebugIcon.java
@@ -2,10 +2,10 @@ package com.blankj.debug;
 
 import android.content.Context;
 import android.content.res.Configuration;
-import android.graphics.Rect;
-import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
@@ -13,13 +13,20 @@ import android.widget.Toast;
 import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 
+import java.lang.ref.WeakReference;
+
 /**
  * A floating debug icon that can be dragged, snapped to screen edges,
  * and opens the debug menu on click.
  */
 public class DebugIcon extends RelativeLayout {
-    private static DebugIcon instance;
-    private final Rect tmpRect = new Rect();
+    private static final Object INSTANCE_LOCK = new Object();
+    private static WeakReference<DebugIcon> instanceRef = new WeakReference<>(null);
+
+    private final OverlayOpener overlayOpener = new OverlayOpener();
+    private final PositionStore positionStore = new PositionStore();
+    private final EdgeSnapper edgeSnapper = new EdgeSnapper();
+
     private int iconResId;
 
     private DebugIcon(@NonNull Context context) {
@@ -29,8 +36,8 @@ public class DebugIcon extends RelativeLayout {
 
     private DebugIcon(@NonNull Context context, @DrawableRes int resId) {
         super(context);
-        this.iconResId = resId;
         init();
+        setIcon(resId);
     }
 
     private void init() {
@@ -42,26 +49,24 @@ public class DebugIcon extends RelativeLayout {
         setOnTouchListener(touchHandler);
 
         // Click opens debug menu
-        setOnClickListener(v -> {
-            if (PermissionUtils.canDrawOverlays(getContext())) {
-                DebugMenu.getInstance().show();
-            } else {
-                PermissionUtils.requestDrawOverlays(getContext(), granted -> {
-                    if (granted) DebugMenu.getInstance().show();
-                    else Toast.makeText(getContext(), R.string.de_permission_tips, Toast.LENGTH_LONG).show();
-                });
-            }
-        });
+        setOnClickListener(v -> overlayOpener.open(getContext()));
     }
 
     /**
      * Obtain (or create) the singleton DebugIcon.
      */
     public static DebugIcon getInstance(@NonNull Context context) {
-        if (instance == null) {
-            instance = new DebugIcon(context.getApplicationContext());
+        DebugIcon inst = instanceRef.get();
+        if (inst == null) {
+            synchronized (INSTANCE_LOCK) {
+                inst = instanceRef.get();
+                if (inst == null) {
+                    inst = new DebugIcon(context.getApplicationContext());
+                    instanceRef = new WeakReference<>(inst);
+                }
+            }
         }
-        return instance;
+        return inst;
     }
 
     /**
@@ -95,68 +100,17 @@ public class DebugIcon extends RelativeLayout {
     }
 
     private void restorePosition() {
-        int x = DebugConfig.getViewX();
-        int y = DebugConfig.getViewY(getContext());
+        int x = positionStore.loadX();
+        int y = positionStore.loadY(getContext());
         setX(x);
         setY(y);
         snapToEdge();
     }
 
     private void savePosition() {
-        DebugConfig.saveViewX((int) getX());
-        DebugConfig.saveViewY((int) getY());
+        positionStore.save((int) getX(), (int) getY());
     }
 
     private void snapToEdge() {
-        post(() -> {
-            View content = getRootView().findViewById(android.R.id.content);
-            if (content == null) return;
-            int parentW = content.getWidth();
-            int halfIcon = getWidth() / 2;
-            float newX = (getX() + halfIcon > parentW / 2f)
-                ? parentW - getWidth()
-                : 0;
-            setX(newX);
-        });
+        edgeSnapper.snap(this);
     }
-
-    /**
-     * Inner touch listener for drag & snap behavior.
-     */
-    private class TouchHandler implements OnTouchListener {
-        private float downX, downY;
-        private int parentW, parentH, statusBarH;
-
-        @Override
-        public boolean onTouch(View v, MotionEvent e) {
-            switch (e.getActionMasked()) {
-                case MotionEvent.ACTION_DOWN:
-                    downX = e.getRawX() - v.getX();
-                    downY = e.getRawY() - v.getY();
-                    View content = getRootView().findViewById(android.R.id.content);
-                    parentW = content.getWidth();
-                    parentH = content.getHeight();
-                    statusBarH = BarUtils.getStatusBarHeight();
-                    v.animate().scaleX(0.9f).scaleY(0.9f).setDuration(100).start();
-                    return true;
-                case MotionEvent.ACTION_MOVE:
-                    float newX = e.getRawX() - downX;
-                    float newY = Math.max(statusBarH, e.getRawY() - downY);
-                    v.setX(clamp(newX, 0, parentW - v.getWidth()));
-                    v.setY(clamp(newY, statusBarH, parentH - v.getHeight()));
-                    return true;
-                case MotionEvent.ACTION_UP:
-                case MotionEvent.ACTION_CANCEL:
-                    snapToEdge();
-                    v.animate().scaleX(1f).scaleY(1f).setDuration(100).start();
-                    return true;
-                default:
-                    return false;
-            }
-        }
-
-        private float clamp(float val, float min, float max) {
-            return Math.max(min, Math.min(max, val));
-        }
-    }
-}
